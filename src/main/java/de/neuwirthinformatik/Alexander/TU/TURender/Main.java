bd4799f927e98ae31aecd1d989e7023aca3564d0
APN-Pucky <APN-Pucky@users.noreply.github.com>
2022-01-15 15:18:14 +0100

    Add jar upload

--- src/main/java/de/neuwirthinformatik/Alexander/TU/TURender/Main.java
+++ src/main/java/de/neuwirthinformatik/Alexander/TU/TURender/Main.java
@@ -3,7 +3,7 @@ package de.neuwirthinformatik.Alexander.TU.TURender;
 public class Main {
 	public static void main(String[] args)
 	{
-		
+		Update.loadUpdate("TUR.jar", "APN-Pucky", "TUR");
 		TUR.startTUR();
 	}
 }

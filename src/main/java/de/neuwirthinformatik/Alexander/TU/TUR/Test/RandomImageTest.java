d89e31f05f249e0b1bcd73d589e7ed9f4df173a2
APN-Pucky <APN-Pucky@users.noreply.github.com>
2022-01-18 11:18:41 +0100

    test ch

--- src/main/java/de/neuwirthinformatik/Alexander/TU/TUR/Test/RandomImageTest.java
+++ src/main/java/de/neuwirthinformatik/Alexander/TU/TUR/Test/RandomImageTest.java
@@ -13,7 +13,7 @@ import de.neuwirthinformatik.Alexander.TU.Render.Render;
 public class RandomImageTest extends CardImageTest {
 	public static void main(String[] args) throws FontFormatException, IOException {
 		GlobalData.init();
-		String name = "DR_F3LL_HOLL_";
+		String name = "DR_F3LL_HOLL___";
 		int seed = name.hashCode();
 		CardInstance ci = Gen.genCardInstance(name,seed);
 		BufferedImage img = new Render().render(ci,new String[] {"","",""},"in.png",Gen.genCardType(ci.getInfo()));

61c50e322804f343a6eaedf29d7ecbffc100b4bb
APN-Pucky <APN-Pucky@users.noreply.github.com>
2022-01-19 09:12:21 +0100

    Allow requirement of cardType

--- src/main/java/de/neuwirthinformatik/Alexander/TU/TUR/Test/RandomCardTest.java
+++ src/main/java/de/neuwirthinformatik/Alexander/TU/TUR/Test/RandomCardTest.java
@@ -6,17 +6,18 @@ import java.io.IOException;
 
 import de.neuwirthinformatik.Alexander.TU.Basic.Card;
 import de.neuwirthinformatik.Alexander.TU.Basic.Card.CardInstance;
+import de.neuwirthinformatik.Alexander.TU.Basic.Card.CardType;
 import de.neuwirthinformatik.Alexander.TU.Basic.Gen;
 import de.neuwirthinformatik.Alexander.TU.Basic.GlobalData;
 import de.neuwirthinformatik.Alexander.TU.Render.Render;
 
-public class RandomImageTest extends CardImageTest {
+public class RandomCardTest extends CardImageTest {
 	public static void main(String[] args) throws FontFormatException, IOException {
 		GlobalData.init();
 		String name = "DR_F3LL_HOLL___";
 		int seed = name.hashCode();
-		CardInstance ci = Gen.genCardInstance(name,seed);
-		BufferedImage img = new Render().render(ci,new String[] {"","",""},"in.png",Gen.genCardType(ci.getInfo()));
+		CardInstance ci = Gen.genCardInstance(name,seed,(c) -> c.getCardType()==CardType.DOMINION);
+		BufferedImage img = new Render().render(ci,new String[] {"","",""},"in.png",ci.getCardType());
 		if(img == null)
 			System.out.println("null img");
 		else
